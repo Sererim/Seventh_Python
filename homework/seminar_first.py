23ac1c9c5d5533e4738ad335b9742a6aa9f74032
Sererim <danielleymanw@gmail.com>
2023-08-07 17:00:15 +0300

    Final commit. Function works correctly.

--- homework/seminar_first.py
+++ homework/seminar_first.py
@@ -5,13 +5,20 @@
 #     - Количество строк и имя файла передаются как аргументы функции.
 from random import randint, random
 
-def fill_file(file_name: str, line: int = -1) -> None:
-    with open(file_name, 'a') as f:
-        f.seek(line)
+def fill_file(file_name: str, line: int = 0) -> None:
+    with open(file_name, 'a', encoding="utf-8") as f:
+        if line == 0:
+            f.seek(line, 2)
+        else:
+            f.seek(line, 0)
+        
         f.write(
-            f"{randint(-1000, 1000)} | {random(-1000, 1001)}"
+            f"{randint(-1000, 1000)} | {randint(-1000, 1000) + random()}\n"
         )
 
 
 if __name__ == "__main__":
-    pass
+    name: str = "test_dir/test_1.txt"
+    fill_file(name)
+    fill_file(name, 10)
+    fill_file(name, 20)
