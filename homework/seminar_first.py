79462f57b5a06f3ac954916df07b40f990c44095
Sererim <danielleymanw@gmail.com>
2023-08-07 16:49:09 +0300

    Basic skeleton for a function.

--- homework/seminar_first.py
+++ homework/seminar_first.py
@@ -3,5 +3,15 @@
 #     - Первое число int, второе - float разделены вертикальной чертой. 
 #     - Минимальное число - -1000, максимальное - +1000. 
 #     - Количество строк и имя файла передаются как аргументы функции.
+from random import randint, random
 
-  
+def fill_file(file_name: str, line: int = -1) -> None:
+    with open(file_name, 'a') as f:
+        f.seek(line)
+        f.write(
+            f"{randint(-1000, 1000)} | {random(-1000, 1001)}"
+        )
+
+
+if __name__ == "__main__":
+    pass
