8d9670742da65b25364e91a0deab58fffac39a1b
Sererim <danielleymanw@gmail.com>
2023-08-08 11:41:41 +0300

    Added function for 5th tasks to __init__.py and main.py

--- homework/__init__.py
+++ homework/__init__.py
@@ -2,6 +2,7 @@ from .seminar_first import fill_file
 from .seminar_second import random_names
 from .seminar_third import read_from_files
 from .seminar_fourth import make_file
+from .seminar_fith import make_extensions_files
 
 __all__ = ['fill_file', 'random_names', 'read_from_files',
-           'make_file']
+           'make_file', 'make_extensions_files']
